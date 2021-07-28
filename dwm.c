591ebdda212f420fcabb638130d38ff8a298904b
Saghya <saghyattila77@gmail.com>
2021-07-28 12:07:43 +0200

    .

--- dwm.c
+++ dwm.c
@@ -761,7 +761,7 @@ drawbar(Monitor *m)
     x = 0;
     for (i = 0; i < LENGTH(tags); i++) {
         w = TEXTW(tags[i]);
-       	drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeTagsSel : SchemeTagsNorm]);
+        drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeTagsSel : SchemeTagsNorm]);
         drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
         if (occ & 1 << i)
             drw_rect(drw, x + boxs, boxs, boxw, boxw,
@@ -781,12 +781,12 @@ drawbar(Monitor *m)
  
     if (w > bh) {
         if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeInfoSel : SchemeInfoNorm]);
+            drw_setscheme(drw, scheme[m == selmon ? SchemeInfoSel : SchemeInfoNorm]);
             drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
             if (m->sel->isfloating)
                 drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
         } else {
-			drw_setscheme(drw, scheme[SchemeInfoNorm]);
+            drw_setscheme(drw, scheme[SchemeInfoNorm]);
             drw_rect(drw, x, 0, w, bh, 1, 1);
         }
     }
