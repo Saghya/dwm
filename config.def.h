4fb747104a8106cc2c3ff80af830bee377e9b186
Saghya <saghyattila77@gmail.com>
2021-09-25 23:39:22 +0200

    shortcut changes

--- config.def.h
+++ config.def.h
@@ -65,29 +65,30 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, NULL };
-static const char *termcmd[]    = { "alacritty", NULL };
-static const char *browser[]    = { "brave", NULL };
+static const char *dmenucmd[]     = { "dmenu_run", "-m", dmenumon, NULL };
+static const char *termcmd[]      = { "alacritty", NULL };
+static const char *browser[]      = { "brave", NULL };
+static const char *file_manager[] = { "alacritty", "-e", "ranger", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
     { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_b,      togglebar,      {0} },
+    { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
     { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
     { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
     { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
     { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-    { MODKEY,                       XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_space,  zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
-    { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    { MODKEY,                       XK_q,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
     { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
     { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-    { MODKEY,                       XK_space,  setlayout,      {0} },
-    { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+    { MODKEY|ShiftMask,             XK_space,  setlayout,      {0} },
+    { MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
     { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
     { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
@@ -103,7 +104,8 @@ static Key keys[] = {
     TAGKEYS(                        XK_4,                      3)
     TAGKEYS(                        XK_5,                      4)
     { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-    { MODKEY|ShiftMask,             XK_f,      spawn,          {.v = browser} },
+    { MODKEY,                       XK_b,      spawn,          {.v = browser} },
+    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = file_manager} },
     { MODKEY,                       XK_x,      spawn,          SHCMD("powermenu") },
     { MODKEY|ShiftMask,             XK_p,      spawn,          SHCMD("passmenu") },
     { 0,                            XK_Print,  spawn,          SHCMD("scrot --silent ~/Pictures/Screenshots/%Y-%m-%d-%T-screenshot.png -e 'xclip -selection clipboard -t image/png -i $f'") },
